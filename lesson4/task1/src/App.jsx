54427cc17647bf707619cff42af5f4d429084ff9
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-11-05 14:33:51 +0200

    test

--- lesson4/task1/src/App.jsx
+++ lesson4/task1/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Counter from './Counter'
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const App = () => {
     return (
-        <Counter/>
-    )
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
 }
 
 export default App
--- lesson4/task1/src/index.jsx
+++ lesson4/task1/src/index.jsx
@@ -1,15 +1,13 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import App from './App'
-import {Provider } from 'react-redux'
 import './index.scss'
-import { store } from "./store";
+
 
 const root = document.querySelector("#root");
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  
+    <App />,
   root
 );
